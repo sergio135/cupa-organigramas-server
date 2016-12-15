f0ddb8acc73938725163baff2e1be47e24fa990c
Sergio Garcia Garcia <sgarcia@cupagroup.com>
2016-12-15 16:46:39 +0100

    ajustes para pruebas

--- models/mysql.js
+++ models/mysql.js
@@ -1,17 +1,9 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  user     : 'nodejs',
+  password : 'nodejs',
+  database : 'nodejs'
 });
- 
-connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
- 
-  console.log('The solution is: ', rows[0].solution);
-});
- 
-connection.end();
+
+module.exports = connection;
--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../models/mysql.js');
 
 /* API Division */
 router.get('/:table', function(req, res, next) {
-    
+
+    console.log(connection);
+    res.json({hola:"asda"});
+
     console.log(req.params);
+
+/*
     connection.connect();
- 
     connection.query('SELECT * FROM usuario', function(err, rows, fields) {
         if (err) throw err;
         console.log(rows);
     });
- 
-    connection.end();
-
-    res.json({object1: "division"});
+    connection.end()
+*/
+    
 });
 
 router.get('/:table/:id', function(req, res, next) {
