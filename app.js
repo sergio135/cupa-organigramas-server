a0212b0777fdf2d9ce7313786956da6540f5cc3a
Sergio <sergio.garcia.seo@gmail.com>
2016-12-29 16:31:19 +0100

    subida para descargar en local

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.set('json spaces', 2);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,47 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../models/mysql.js');
 
-
+var hola = {
+    "nombre": "Staff Boxx",
+    "padre": null,
+    "pagina": 0,
+    "funciones": "",
+    "user":{
+            "nombre": "Sergio García",
+            "user": "sgarcia",
+            "foto": "sergio-garcia.jpg",
+            "id": 27,
+            "createdAt": "2015-10-16T08:48:14.000Z",
+            "updatedAt": "2015-10-21T11:00:04.000Z"
+            },
+    "id": 65,
+    "createdAt": "2015-10-16T08:45:23.000Z",
+    "updatedAt": "2015-10-20T15:32:02.000Z"
+  };
 
 /* API Division */
+
+router.get('/pruebas', (req, res, next) => {
+    connection.query('SELECT * FROM division LIMIT 4', (err, rows, fields) => {
+        if (err) throw err;
+        rows.map( (i)=> {
+            if (i.user) {
+                connection.query('SELECT * FROM usuario WHERE id = ?', [i.user], (err, rows, fields) => {
+                    if (err) throw err;
+                    console.log(rows);
+                    i.user = rows[0];
+                });
+            }
+        });
+        setTimeout(function() {
+            res.json(rows);
+        }, 2000);
+    });
+});
+
 router.get('/:table', (req, res, next) => {
     consultarAll(req.params.table, req.query, (salida) => {
+
         res.json(salida);
     });
 });
@@ -36,12 +72,36 @@ function consultarAll(params, query, callback) {
     if ( isFull(query) ) {
         connection.query('SELECT * FROM ?? WHERE ?', [params, query], (err, rows, fields) => {
             if (err) throw err;
-            callback(rows);
+            let length = rows.length;
+            let counter = 0;
+            let that = rows;
+            rows.map((i)=> {
+                connection.query('SELECT * FROM usuario WHERE id = ?', [i.user], (err, rows, fields) => {
+                    if (err) throw err;
+                    i.user = rows[0];
+                    counter ++;
+                    if (counter === length) {
+                        callback(that);
+                    }
+                });
+            });
         });
     } else {
         connection.query('SELECT * FROM ??', [params], (err, rows, fields) => {
             if (err) throw err;
-            callback(rows);
+            let length = rows.length;
+            let counter = 0;
+            let that = rows;
+            rows.map((i)=> {
+                connection.query('SELECT * FROM usuario WHERE id = ?', [i.user], (err, rows, fields) => {
+                    if (err) throw err;
+                    i.user = rows[0];
+                    counter ++;
+                    if (counter === length) {
+                        callback(that);
+                    }
+                });
+            });
         });
     }
 }
@@ -49,7 +109,19 @@ function consultarAll(params, query, callback) {
 function consultarById(params, params2, callback) {
     connection.query('SELECT * FROM ?? WHERE id = ?', [params, params2], (err, rows, fields) => {
         if (err) throw err;
-        callback(rows);
+        let length = rows.length;
+        let counter = 0;
+        let that = rows;
+        rows.map((i)=> {
+            connection.query('SELECT * FROM usuario WHERE id = ?', [i.user], (err, rows, fields) => {
+                if (err) throw err;
+                i.user = rows[0];
+                counter ++;
+                if (counter === length) {
+                    callback(that);
+                }
+            });
+        });
 });}
 
 function addNew(params, params2, callback) {
